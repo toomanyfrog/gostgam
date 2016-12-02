1823caf14fda9f6b6fc482437acee5d6006d1a8b
yeojoey <yeojoey@gmail.com>
2016-12-02 17:56:26 +0800

    Add Camera and Player controllers

---
+++ CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float xMin, xMax, yMin, yMax;
+
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - player.transform.position;
+	
+	}
+
+	void LateUpdate () {
+
+		Vector3 newPos = player.transform.position + offset;
+		this.transform.position = newPos;
+
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1;
+
+	private float speedMult = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorz = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+
+
+		Vector2 movement = new Vector2 (moveHorz, moveVert);
+
+		transform.Translate (movement * speed * Time.deltaTime);
+
+		Debug.Log (moveHorz + ", " + moveVert);
+
+		
+
+	}
+}
