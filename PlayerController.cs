b196f96ea87e2334de8666bacb8c0d059efb04bf
yeojoey <yeojoey@gmail.com>
2016-12-02 22:00:45 +0800

    Move unrelated stuff from PlayerController

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5;
+	public float goodZoneTime = 3;
+
+	public float goodZoneInitOpacity = 0.274f;
+	public float goodZoneEndOpacity = 0.6f;
+
+	private float timeInGoodZone = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +20,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
 		float moveHorz = Input.GetAxis ("Horizontal");
 		float moveVert = Input.GetAxis ("Vertical");
 
@@ -22,4 +29,64 @@ public class PlayerController : MonoBehaviour {
 		transform.Translate (movement * speed * Time.deltaTime);
 
 	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.gameObject.tag == "Bad Zone") {
+
+			timeInGoodZone = 0;
+			other.gameObject.transform.parent.transform.position = new Vector3 (Random.Range(-10, 10), Random.Range(-10, 10));
+
+		}
+
+
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+		if (other.gameObject.tag == "Good Zone") {
+
+			StartCoroutine (MakeOpaque(other.gameObject.GetComponent<SpriteRenderer>(), goodZoneTime));
+			timeInGoodZone += Time.deltaTime;
+
+		}
+
+		if (timeInGoodZone >= goodZoneTime) {
+
+			other.gameObject.transform.parent.transform.position = new Vector3 (Random.Range(-10, 10), Random.Range(-10, 10));
+			SpriteRenderer sr = other.gameObject.GetComponent<SpriteRenderer> ();
+			sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, goodZoneInitOpacity);
+		
+		}
+
+
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
+		if (other.gameObject.tag == "Good Zone") {
+
+			StopCoroutine ("MakeOpaque");
+			SpriteRenderer sr = other.gameObject.GetComponent<SpriteRenderer> ();
+			sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, goodZoneInitOpacity);
+			timeInGoodZone = 0;
+
+		}
+
+	}
+
+	IEnumerator MakeOpaque(SpriteRenderer sr, float duration) {
+		
+
+		for (float t = 0.0f; t < 1.0f; t+= Time.deltaTime/duration) {
+
+			Debug.Log (1);
+
+			sr.color = new Color (sr.color.r, sr.color.g, sr.color.b, Mathf.Lerp(0, 1, duration));
+			yield return null;
+		}
+		
+	}
+		
+
 }
